83c298fd60e290a805b0dc36c7144caa6fb778ad
Dev <prodevonline@gmail.com>
2021-05-24 14:55:07 +0530

    gh

--- dependency_check.py
+++ dependency_check.py
@@ -4,6 +4,12 @@ import shutil
 
 VIRTUALENV = None
 
+#Files Check
+status = os.system(f'cat {os.getcwd()}/requirements.txt')
+if(status!=0):
+    print("Requirements.txt not found")
+    exit(status)
+
 if platform.system()=="Linux":
 
     if shutil.which('pip') == None :
@@ -15,15 +21,24 @@ if platform.system()=="Linux":
         if 'env' in os.listdir(os.getcwd()):
             if 'activate' in os.listdir(f"{os.getcwd()}/env/bin"):
                 print("Virtual env exists")
-                print(os.system(f'env/bin/easy_install `cat {os.getcwd()}/requirements.txt`'))
+                print(os.system(f'sudo -S env/bin/easy_install `cat {os.getcwd()}/requirements.txt`'))
         else:
             os.system('sudo -S python3 -m venv env')
 
     root_dir = os.getcwd().rsplit('/', 1)[0]
     django_dir = None
     for dir in os.listdir(root_dir):
-        if 'manage.py' in os.listdir(f"{root_dir}/{dir}"):
-            django_dir = f"{root_dir}/{dir}"
-            break
-    print('#####################')
+        try:
+            if 'manage.py' in os.listdir(f"{root_dir}/{dir}"):
+                django_dir = f"{root_dir}/{dir}"
+                break
+        except NotADirectoryError:
+            pass
+
+    if django_dir == None:
+        print('#####################\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
+        print("Django project not found. Place this folder outside django project folder and rerun")
+        exit()
+
+    print('#####################\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
     os.system(f'env/bin/python3 form-data-generator.py')
